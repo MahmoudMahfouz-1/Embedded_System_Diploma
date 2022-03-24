6a4d2b0dd2159008c5ff31d94ba856874b612c6e
MahmoudMahfouz-1 <60852996+MahmoudMahfouz-1@users.noreply.github.com>
2022-03-24 21:48:16 +0200

    Lesson3 Assignment

---
+++ Unit3_Embedded_C/Lesson3/Toggle_Led Unit3_lesson3/startup.c
@@ -0,0 +1,57 @@
+// Startup.c
+// Eng.Mahmoud Essam
+#define Stack_Top 0x20001000
+#include "stdint.h"
+void Reset_Handler(void);
+void Defaut_Handler(void);
+extern int main(void);
+void Defaut_Handler(void)
+{
+	Reset_Handler();
+}
+
+void NMI_Handler(void) __attribute__((weak, alias("Defaut_Handler")));
+void H_fault_Handler(void) __attribute__((weak, alias("Defaut_Handler")));
+void MM_Fault_Handler(void) __attribute__((weak, alias("Defaut_Handler")));
+void Bus_Fault(void) __attribute__((weak, alias("Defaut_Handler")));
+void Usage_Fault_Handler(void) __attribute__((weak, alias("Defaut_Handler")));
+extern unsigned int _stack_top; 
+uint32_t vectors[]__attribute__((section(".vectors"))) =
+{
+	(uint32_t) &_stack_top,
+	(uint32_t) &Reset_Handler,
+	(uint32_t) &NMI_Handler,
+	(uint32_t) &H_fault_Handler,
+	(uint32_t) &MM_Fault_Handler,
+	(uint32_t) &Bus_Fault,
+	(uint32_t) &Usage_Fault_Handler,
+};
+extern unsigned int _E_TEXT_;
+extern unsigned int _S_DATA_;
+extern unsigned int _E_DATA_;
+extern unsigned int _S_BSS_;
+extern unsigned int _E_BSS_;
+
+
+void Reset_Handler(void)
+{
+	// Copy .data section from flash to sram
+	unsigned int DATA_size = (unsigned char*)&_E_DATA_ - (unsigned char*)&_S_DATA_;
+	unsigned char* p_src = (unsigned char*)&_E_TEXT_; // end of flash
+	unsigned char* p_dst = (unsigned char*)&_S_DATA_; // start of sram
+	for(int i = 0; i < DATA_size ; i++)
+	{
+		*p_dst++ = *p_src++;
+	}
+	// Init .bss section
+	unsigned int BSS_size = (unsigned char*)&_E_BSS_ - (unsigned char*)&_S_BSS_ ;
+	p_dst = (unsigned char*)&_S_BSS_;
+	for(int i = 0; i < DATA_size ; i++)
+	{
+		*p_dst++ = (unsigned char)0;
+	}
+	// Jumb main
+	
+
+	main();
+}
